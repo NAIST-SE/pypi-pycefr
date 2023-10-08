66f7ec5833e5319b6c2dd09f6b359f053c2a6357
R.G Kula <raula.kilarouge@gmail.com>
2023-10-08 20:52:03 +0900

    fix error

--- src/pycefr/levels.py
+++ src/pycefr/levels.py
@@ -121,11 +121,14 @@ def level_Dict(self):
         
         #-- Check for lists inside dictionary dictionaries
         for i in range(0, len(self.node.values)):
-                if 'ast.List' in str(self.node.values[i].values):
-                    numList += str(self.node.values[i].values).count('ast.List')
-                    self.level= dictLevel['Dict'][3]['with-dict-list']
-                    self.clase = (str(numList) + ' List in ' + str(numDict) +
+                try:
+                    if 'ast.List' in str(self.node.values[i].values):
+                        numList += str(self.node.values[i].values).count('ast.List')
+                        self.level= dictLevel['Dict'][3]['with-dict-list']
+                        self.clase = (str(numList) + ' List in ' + str(numDict) +
                             'Dictionary of Dictionary')
+                except:
+                    pass
             
     #-- Check for lists
     elif 'ast.List' in str(self.node.values):
